ddd58642f187f4f2d14e5e46348f89da9676dceb
ataknz <atakanzzengin@gmail.com>
2021-02-02 23:44:20 +0300

    Change padding

--- pages/og.js
+++ pages/og.js
@@ -7,7 +7,7 @@ const og = () => {
   const { title } = router.query;
 
   return (
-    <div className="flex flex-col items-center pt-20">
+    <div className="flex flex-col items-center pt-28">
       <ProfileImage size={150}></ProfileImage>
       <h1 className="text-9xl font-extrabold text-gray-800 robotoSlab">
         {title}
