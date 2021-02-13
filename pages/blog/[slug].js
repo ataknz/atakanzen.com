4cbc2339bf41e12564db60c26e9345332d0fd4ee
ataknz <atakanzzengin@gmail.com>
2021-02-13 14:04:52 +0300

    Add SEO meta tags for single blog page.

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -3,6 +3,8 @@ import Link from "next/link";
 import { getBlogTable, getSinglePage } from "../../lib/posts";
 import { Nav } from "../../components/Nav";
 import { NotionRenderer } from "react-notion";
+import { NextSeo } from "next-seo";
+import { getOpenGraphImage } from "../../utils/og-image";
 
 export const getStaticPaths = async () => {
   const blogTable = await getBlogTable(process.env.BLOG_TABLE_ID);
@@ -48,14 +50,37 @@ export const getStaticProps = async ({ params }) => {
 const blogPost = ({ blogPost, blocks }) => {
   return (
     <>
+      <NextSeo
+        title={blogPost.title}
+        description={blogPost.excerpt}
+        openGraph={{
+          type: "article",
+          images: [getOpenGraphImage(blogPost.title)],
+          article: {
+            publishedTime: new Date(blogPost.date).toISOString(),
+          },
+        }}
+        twitter={{
+          handle: "@atakanzzengin",
+          cardType: "summary_large_image",
+        }}
+        canonical={`https://zengin.me/blog/${blogPost.slug}`}
+        titleTemplate="%s • Atakan Zengin • Blog"
+        additionalMetaTags={[
+          {
+            name: "date",
+            content: new Date(blogPost.date).toDateString(),
+          },
+        ]}
+      ></NextSeo>
       <Nav></Nav>
       <div className="px-4 mt-8 mb-12 md:mt-12 md:mb-18">
         <h1 className="mb-2 text-2xl font-bold md:text-3xl sm:text-center rubik">
           {blogPost.title}
         </h1>
         <div className="text-gray-600 sm:text-center rubik">
-          <time dateTime={new Date(blogPost.date).toISOString()}>
-            {blogPost.date}
+          <time dateTime={new Date(blogPost.date).toDateString()}>
+            {new Date(blogPost.date).toDateString()}
           </time>
         </div>
       </div>
