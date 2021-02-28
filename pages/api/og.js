091a1e68713e32e02ff904b3a46e9aaeaa561f29
ataknz <atakanzzengin@gmail.com>
2021-02-28 11:31:54 +0300

    Change waitUntil event.

--- pages/api/og.js
+++ pages/api/og.js
@@ -15,7 +15,7 @@ export default async (req, res) => {
   });
 
   const url = `https://zengin.me/og?title=${title}`;
-  await page.goto(url, { waitUntil: "load" });
+  await page.goto(url, { waitUntil: "domcontentloaded" });
   const data = await page.screenshot({
     type: "png",
   });
