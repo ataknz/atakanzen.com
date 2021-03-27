c1d081f44c59a46c5d1f3074af06742be0b7e217
ataknz <atakanzzengin@gmail.com>
2021-03-27 17:46:33 +0300

    Implement Hamburger menu for Mobile
    - Add custom animation and Burger component.
    - Because of the layout of my components, I had to find a dodgy way to
      prevent scrolling when the menu is active, I'll find a way to improve
      it.

---
+++ components/Menu.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { ActiveLink } from "./ActiveLink";
+
+const Menu = () => {
+  return (
+    <div className="h-screen w-full fixed flex flex-col items-end justify-center bg-white dark:bg-raisinBlack z-40">
+      <ul className="text-5xl pr-10">
+        <li className="mb-9">
+          <ActiveLink activeClassName="underline" href="/">
+            <a>Home</a>
+          </ActiveLink>
+        </li>
+        <li>
+          <ActiveLink activeClassName="underline" href="/blog">
+            <a>Blog</a>
+          </ActiveLink>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export { Menu };
--- components/Nav.js
+++ components/Nav.js
@@ -1,26 +1,47 @@
+import React, { useState, useEffect } from "react";
 import { ActiveLink } from "./ActiveLink";
 import { ThemeSwitch } from "./ThemeSwitch";
+import { Burger } from "./Burger";
+import { Menu } from "./Menu";
 
 export const Nav = () => {
+  const [active, setActive] = useState(false);
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  });
+
+  // This seems very dodgy I know, will find a better way.
+  if (mounted) {
+    active
+      ? (document.querySelector("body").style.overflow = "hidden")
+      : (document.querySelector("body").style.overflow = "auto");
+  }
+
   return (
-    <div className="flex items-center justify-center mb-12">
-      <nav className="justify-between px-3 py-3 z-50 bg-white border-b border-gray-200 dark:border-gray-500 dark:bg-raisinBlack bg-opacity-100 w-full fixed top-0">
-        <ul className="flex items-center justify-center">
-          <li className="absolute left-4">
+    <>
+      <div className="flex items-center justify-center">
+        <nav className="flex items-center justify-between md:justify-center px-3 py-3 z-50 bg-white border-b border-gray-200 dark:border-gray-500 dark:bg-raisinBlack w-full fixed top-0">
+          <ul className="hidden md:flex items-center ">
+            <li className="hidden md:block">
+              <ActiveLink activeClassName="underline" href="/">
+                <a>Home</a>
+              </ActiveLink>
+            </li>
+            <li className="pl-4 hidden md:block">
+              <ActiveLink activeClassName="underline" href="/blog">
+                <a>Blog</a>
+              </ActiveLink>
+            </li>
+          </ul>
+          <Burger active={active} setActive={setActive} />
+          <div className="absolute right-3 z-50">
             <ThemeSwitch />
-          </li>
-          <li>
-            <ActiveLink activeClassName="underline" href="/">
-              <a>Home</a>
-            </ActiveLink>
-          </li>
-          <li className="pl-4">
-            <ActiveLink activeClassName="underline" href="/blog">
-              <a>Blog</a>
-            </ActiveLink>
-          </li>
-        </ul>
-      </nav>
-    </div>
+          </div>
+        </nav>
+      </div>
+      {active && <Menu />}
+    </>
   );
 };
