8efec02ce2e6cd513f980d3e4a1cba23237244fb
Atakan Zengin <atakanzzengin@gmail.com>
2022-04-29 10:06:59 +0200

    Center index hero section

--- components/Layout.js
+++ components/Layout.js
@@ -1,18 +1,24 @@
-import { Hero } from './Hero'
-import { Nav } from './Nav'
+import { Hero } from "./Hero";
+import { Nav } from "./Nav";
 
 const Layout = ({ children, index }) => {
   return (
     <>
       <Nav />
-      {index && (
-        <div>
-          <Hero />
-        </div>
-      )}
-      {<div>{children}</div>}
+      <div
+        className={
+          index ? `h-screen flex flex-col items-center justify-center` : ""
+        }
+      >
+        {index && (
+          <div>
+            <Hero />
+          </div>
+        )}
+        {<div>{children}</div>}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
