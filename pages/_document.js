855ca63c7bd4c87c626e0e3489e8b738aaa0fbc9
Atakan Zengin <atakanzzengin@gmail.com>
2023-05-31 15:51:34 +0200

    Remove GA tag script

--- pages/_document.js
+++ pages/_document.js
@@ -32,29 +32,6 @@ export default class MyDocument extends Document {
           <link rel="shortcut icon" href="/favicon.ico?v=693qL8BNpN" />
           <meta name="msapplication-TileColor" content="#faa916" />
           <meta name="theme-color" content="#ffffff" />
-          <meta
-            name="google-site-verification"
-            content="QEyNh-IOVjpqNQt0wdWhxB5w-kDDNvBZ2l_iNsGt_i4"
-          />
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                {/* Global site tag (gtag.js) - Google Analytics */}
-
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-
-                gtag('config', '${gaTrackingID}', {
-                  page_path: window.location.pathname,
-                });
-             `,
-            }}
-          />
         </Head>
         <body className="bg-white text-gray-800 dark:bg-raisinBlack dark:text-gray-200">
           <Main />
